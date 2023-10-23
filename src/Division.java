ce7b91cef701ef3aa37c689adf20cbe42e3df288
alfmorais <alfredneto@hotmail.com>
2023-10-23 08:19:08 -0300

    Feat: Output and operations to display on screen

---
+++ src/Division.java
@@ -0,0 +1,19 @@
+
+public class Division {
+
+	public static void main(String[] args) {
+		int a, b;
+		double result;
+		double resultExact;
+		
+		a = 5;
+		b = 2;
+		
+		result = a / b;
+		resultExact = (double) a / b;
+		
+		System.out.println(result);
+		System.out.println(resultExact);
+	}
+
+}
