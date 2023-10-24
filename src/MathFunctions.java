572dbdd8cdf1dd6901255afd43fbcac63bb78e53
alfmorais <alfredneto@hotmail.com>
2023-10-24 08:27:14 -0300

    Feat: mathematics functions in standard java programming.

---
+++ src/MathFunctions.java
@@ -0,0 +1,16 @@
+
+public class MathFunctions {
+
+	public static void main(String[] args) {
+		double valueA = 2.0;
+		double valueB = 4.0;
+		double sqrt = Math.sqrt(valueA);
+		double pow = Math.pow(valueA, valueB);
+		double abs = Math.abs(valueA);
+
+		System.out.println("Raiz quadrada de 2.0: " + sqrt);
+		System.out.println("Potência de 2.0 elevado a 4.0: " + pow);
+		System.out.println("Valor absoluto de 2.0: " + abs);
+	}
+
+}
